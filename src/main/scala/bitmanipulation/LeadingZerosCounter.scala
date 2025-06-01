5ac2103bb0f34efc8fc26a2591a9cf9fbc519a0a
Ahmed Shora <ahmedwaleedshora123@gmail.com>
2025-06-01 12:58:30 +0200

    LeadingZerosCounter Done

--- src/main/scala/bitmanipulation/LeadingZerosCounter.scala
+++ src/main/scala/bitmanipulation/LeadingZerosCounter.scala
@@ -13,7 +13,22 @@ abstract class AbstractLeadingZerosCounter(bitWidth: Int) extends Module {
 // You may expect bitWidth to be a power of two.
 class LeadingZerosCounter(bitWidth: Int)
     extends AbstractLeadingZerosCounter(bitWidth) {
-
-  ??? // TODO: implement Task 1.1 here
+   
+   if(bitWidth == 1){
+    io.result := ~(io.input(0))
+   }
+   else{
+    val upperZeroes = Module(new LeadingZerosCounter(bitWidth/2))
+    upperZeroes.io.input := io.input(bitWidth-1,bitWidth/2) 
+    val lowerZeroes = Module(new LeadingZerosCounter(bitWidth/2))
+     lowerZeroes.io.input := io.input((bitWidth/2)-1,0)
+  
+    when(io.input(bitWidth-1 , bitWidth/2) === 0.U){ 
+     io.result :=  (lowerZeroes.io.result +& (bitWidth/2).U)(log2Ceil(bitWidth + 1)-1,0) 
+      }
+      .otherwise{
+       io.result := upperZeroes.io.result.asUInt
+      }
+   }
 
 }
