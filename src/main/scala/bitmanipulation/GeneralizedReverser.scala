ec9092055fe572b4922a842ceb16906c6298f489
YousefFarag <yofa00002@stud.uni-saarland.de>
2025-06-02 17:31:03 +0200

    ana etbdant 5alas ya 3mr

--- src/main/scala/bitmanipulation/GeneralizedReverser.scala
+++ src/main/scala/bitmanipulation/GeneralizedReverser.scala
@@ -11,9 +11,47 @@ abstract class AbstractGeneralizedReverser(bitWidth: Int) extends Module {
   })
 }
 
+object Util
+{
+  def createAlternatingString(bitWidth: Int, start: BigInt, alternateWhen: Int) : UInt =
+  {
+    var result = BigInt(0);
+    var startMut = start;
+    for(_ <- 0 until bitWidth / alternateWhen)
+    {
+      for(j <- 0 until alternateWhen)
+      {
+        result = (result | start) << 1;
+      }
+      startMut = if (startMut == 0) 1 else 0;
+    }
+
+    result.U(bitWidth.U)
+  }
+}
+
 class GeneralizedReverser(bitWidth: Int)
     extends AbstractGeneralizedReverser(bitWidth) {
+    import Util._
 
-  ??? // TODO: implement Task 1.2 here
+    val count = log2Ceil(bitWidth);
+    val stages = Wire(Vec(count+1, UInt(bitWidth.W)));
+    stages(0) := io.input;
+
+    if(bitWidth == 1){
+      io.result := io.input(0); 
+    }
+    for(i <- 0 until count)
+    {
+      val shiftSize = 1 << i;
+       when(io.pattern(i))
+       {
+        stages(i+1) := (stages(i) & Util.createAlternatingString(bitWidth, 0, i +1) << shiftSize) | (stages(i) & Util.createAlternatingString(bitWidth, 1, i +1) >> shiftSize);
+       }
+       .otherwise{
+        stages(i+1) := stages(i); 
+       }
+    }
+  io.result := stages(count);
 
 }
