54e359154189cb5bb60b5b1dfb087a1c55650bad
YousefFarag <yofa00002@stud.uni-saarland.de>
2025-06-19 23:18:48 +0200

    reverted the GREV

--- src/main/scala/bitmanipulation/GeneralizedReverser.scala
+++ src/main/scala/bitmanipulation/GeneralizedReverser.scala
@@ -1,4 +1,5 @@
 package bitmanipulation
+
 import chisel3._
 import chisel3.util._
 
@@ -10,57 +11,47 @@ abstract class AbstractGeneralizedReverser(bitWidth: Int) extends Module {
   })
 }
 
-object Util {
-  // Generate mask for GREV operation at stage i
-  // For stage i, we need to create a mask that selects every other group of 2^i bits
-  def createGrevMask(bitWidth: Int, stage: Int, selectLower: Boolean): UInt = {
-    val groupSize = 1 << stage
-    val pairSize = groupSize * 2
-    var result = BigInt(0)
-    
-    var pos = 0
-    while (pos < bitWidth) {
-      // For each pair of groups, select either the lower or upper group
-      val groupStart = if (selectLower) pos else pos + groupSize
-      val groupEnd = scala.math.min(groupStart + groupSize, bitWidth)
-      
-      for (bit <- groupStart until groupEnd) {
-        if (bit < bitWidth) {
-          result = result | (BigInt(1) << bit)
-        }
+object Util
+{
+  def createAlternatingString(bitWidth: Int, start: BigInt, alternateWhen: Int) : UInt =
+  {
+    var result = BigInt(0);
+    var startMut = start;
+    for(_ <- 0 until bitWidth / alternateWhen)
+    {
+      for(j <- 0 until alternateWhen)
+      {
+        result = (result | start) << 1;
       }
-      
-      pos += pairSize
+      startMut = if (startMut == 0) 1 else 0;
     }
-    
-    result.U(bitWidth.W)
+
+    result.U(bitWidth.U)
   }
 }
 
-class GeneralizedReverser(bitWidth: Int) extends AbstractGeneralizedReverser(bitWidth) {
-  import Util._
-  
-  val count = log2Ceil(bitWidth)
-  val stages = Wire(Vec(count + 1, UInt(bitWidth.W)))
-  stages(0) := io.input
-  
-  if (bitWidth == 1) {
-    io.result := io.input
-  } else {
-    for (i <- 0 until count) {
-      val shiftSize = 1 << i
-      
-      when(io.pattern(i)) {
-        // Generate masks for this stage
-        val lowerMask = Util.createGrevMask(bitWidth, i, true)  // Select lower groups
-        val upperMask = Util.createGrevMask(bitWidth, i, false) // Select upper groups
-        
-        stages(i + 1) := ((stages(i) & lowerMask) << shiftSize) | ((stages(i) & upperMask) >> shiftSize)
-      }.otherwise {
-        stages(i + 1) := stages(i)
-      }
+class GeneralizedReverser(bitWidth: Int)
+    extends AbstractGeneralizedReverser(bitWidth) {
+    import Util._
+
+    val count = log2Ceil(bitWidth);
+    val stages = Wire(Vec(count+1, UInt(bitWidth.W)));
+    stages(0) := io.input;
+
+    if(bitWidth == 1){
+      io.result := io.input(0); 
     }
-    
-    io.result := stages(count)
-  }
+    for(i <- 0 until count)
+    {
+      val shiftSize = 1 << i;
+       when(io.pattern(i))
+       {
+        stages(i+1) := (stages(i) & Util.createAlternatingString(bitWidth, 0, i +1) << shiftSize) | (stages(i) & Util.createAlternatingString(bitWidth, 1, i +1) >> shiftSize);
+       }
+       .otherwise{
+        stages(i+1) := stages(i); 
+       }
+    }
+  io.result := stages(count);
+
 }
